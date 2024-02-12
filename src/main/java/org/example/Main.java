05d3c3eadfe5def09cb746cc26f3454fddd184ef
Jahongir <sjahongir22@gmail.com>
2024-02-12 12:12:37 +0500

    connect 3- step

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver"); // 1 - step
-        DriverManager.getConnection("jdbc:postgresql://localhost:5432/learn_java_jdbc", "postgres", "1"); // 2 - step
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/learn_java_jdbc", "postgres", "1"); // 2 - step
+        Statement statement = connection.createStatement(); // 3 - step
 
     }
 }
