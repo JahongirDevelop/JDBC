0da70ba6d824a9d183d2d94d95b8c8c4b7107b86
Jahongir <sjahongir22@gmail.com>
2024-02-13 16:15:13 +0500

    get data from db

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,28 @@
 package org.example;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver"); // 1 - step (Register Driver -> driverni ro'yxatdan o'tkazish)
         Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/learn_java_jdbc", "postgres", "1"); // 2 - step (Get connection -> db blan connection o'rnatish)
         Statement statement = connection.createStatement(); // 3 - step (Create Statement -> statement yaratish)
-        ResultSet resultSet = statement.executeQuery("select * from students"); // 4 - step (Execute Query -> db ga SQL ni jo'natish)
+        ResultSet resultSet = statement.executeQuery("select * from movie"); // 4 - step (Execute Query -> db ga SQL ni jo'natish)
 
         while (resultSet.next()){
-            System.out.println(resultSet.getInt("id") + " -> " + resultSet.getString("name"));
-        }
+//            System.out.println(resultSet.getInt("id") + " -> " + resultSet.getString("name"));
+            Integer id = resultSet.getInt("id");
+            String title = resultSet.getString("title");
+            Long duration = resultSet.getLong("duration");
+            LocalDateTime createdDate = resultSet.getTimestamp("created_date").toLocalDateTime();
+            LocalDate publishDate = resultSet.getDate("publish_date").toLocalDate();
+            Double rating = resultSet.getDouble("rating");
+            System.out.println(id + " " + title + " " + duration + " " + createdDate + " " + publishDate + " " + rating);
 
+
+        }
         connection.close(); // 5 - step (Colose Connection -> db bilan yaratilgan connectionni uzish)
 
 
