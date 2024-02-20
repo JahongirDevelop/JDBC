583c4465921625ed7aff28bed7b784fc480a776a
JahongirDevelop <sjahongir221@gmail.com>
2024-02-20 16:25:13 +0500

    selectById

--- src/main/java/org/example/StatementExamples.java
+++ src/main/java/org/example/StatementExamples.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
@@ -8,7 +9,8 @@ import java.time.LocalDate;
 public class StatementExamples {
     public static void main(String[] args) {
 //        createTable();
-        insertQuery("","","", LocalDate.of(2024,02,20));
+//        insertQuery("Learn","JDBC","Alish", LocalDate.of(2021,03,23));
+        selectById(2);
     }
     public static void createTable(){
         try {
@@ -32,11 +34,29 @@ public class StatementExamples {
         try {
             Connection connection = DBUtil.getConnection();
             Statement statement = connection.createStatement();
-            String sql = "insert into article(title, content, author_name, publish_date) values ('Java', 'Java lessons', 'Big-man','2024-02-20')";
+            String sql = "insert into article(title, content, author_name, publish_date) values ('%s', '%s', '%s', '%s')";
+            sql =  String.format(sql, title, content, author_name, publishDate.toString());
             statement.executeUpdate(sql);
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void selectById(Integer ide){
+        try {
+        Connection connection = DBUtil.getConnection();
+            Statement statement = connection.createStatement();
+            String sql = "select * from article where id = " + ide + "; ";
+            ResultSet resultSet = statement.executeQuery(sql);
+            if (resultSet.next()){
+                int idd = resultSet.getInt("id");
+                System.out.println("id: " + idd);
+
+            }
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
