39171a9f6750ab690cd684fec63a1975f75f9ebf
JahongirDevelop <sjahongir221@gmail.com>
2024-02-20 11:27:38 +0500

    insertQuery

--- src/main/java/org/example/StatementExamples.java
+++ src/main/java/org/example/StatementExamples.java
@@ -3,10 +3,12 @@ package org.example;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 
 public class StatementExamples {
     public static void main(String[] args) {
-        createTable();
+//        createTable();
+        insertQuery("","","", LocalDate.of(2024,02,20));
     }
     public static void createTable(){
         try {
@@ -23,6 +25,18 @@ public class StatementExamples {
             connection.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void insertQuery(String title, String content, String author_name, LocalDate publishDate){
+        try {
+            Connection connection = DBUtil.getConnection();
+            Statement statement = connection.createStatement();
+            String sql = "insert into article(title, content, author_name, publish_date) values ('Java', 'Java lessons', 'Big-man','2024-02-20')";
+            statement.executeUpdate(sql);
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
