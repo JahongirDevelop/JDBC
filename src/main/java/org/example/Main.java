7e97c06d4cf1907a70a4c7faea58eba964025305
JahongirDevelop <sjahongir221@gmail.com>
2024-02-22 22:17:38 +0500

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,7 @@
 package org.example;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 //        List<Movie> allMovie = getAllMovie();
