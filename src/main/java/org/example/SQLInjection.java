d68065dd74f04e23d6ca66037b709a40253072cd
JahongirDevelop <sjahongir221@gmail.com>
2024-02-25 20:43:00 +0500

    SQLInjection

--- src/main/java/org/example/SQLInjection.java
+++ src/main/java/org/example/SQLInjection.java
@@ -6,23 +6,19 @@ import java.sql.Statement;
 
 public class SQLInjection {
     public static void main(String[] args) {
-
-
-
-//        Profile profile = authorization("alish", "kalish");
-//        System.out.println(profile);
+        Profile profile = authorization("alish", "kalish");
+        System.out.println(profile);
     }
 
-//    public static Profile authorization(String login, String password){
-//        try {
-//        Connection connection = DBUtil.getConnection();
-//        Statement statement = connection.createStatement();
-//
-//        String sql = "select * from profile where login = '%s' and password = '%s' ";
-//        statement.executeQuery(sql);
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//    }
+    public static Profile authorization(String login, String password){
+        try {
+            Connection connection = DBUtil.getConnection();
+            Statement statement = connection.createStatement();
+            String sql = "select * from profile where login = '%s' and password = '%s' ";
+            statement.executeQuery(sql);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
