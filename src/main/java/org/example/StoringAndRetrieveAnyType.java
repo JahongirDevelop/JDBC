d8ac91c9f312b6d151951d591be9b004b22a8079
JahongirDevelop <sjahongir221@gmail.com>
2024-02-26 21:52:39 +0500

    SQLInjection

--- src/main/java/org/example/StoringAndRetrieveAnyType.java
+++ src/main/java/org/example/StoringAndRetrieveAnyType.java
@@ -58,6 +58,7 @@ public class StoringAndRetrieveAnyType {
             throw new RuntimeException(e);
         }
     }
+
     public static void createTable() {
         try {
             Connection connection = DBUtil.getConnection();
