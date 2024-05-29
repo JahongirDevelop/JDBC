ebaf2a363e507480639f37073c71d25faffaba9c
Jahongir Sobirjonov <131335862+JahongirDevelop@users.noreply.github.com>
2024-05-29 12:44:41 +0500

    Update DBUtil.java

--- src/main/java/org/example/DBUtil.java
+++ src/main/java/org/example/DBUtil.java
@@ -3,9 +3,12 @@ import lombok.SneakyThrows;
 import java.sql.Connection;
 import java.sql.DriverManager;
 public class DBUtil {
+    public static String dbUrl = System.getenv("DB_URL");
+    public static String dbUser = System.getenv("DB_USER");
+   public static String dbPassword = System.getenv("DB_PASSWORD");
     @SneakyThrows
     public static Connection getConnection() {
         Class.forName("org.postgresql.Driver"); // 1 - step (Register Driver -> driverni ro'yxatdan o'tkazish) Buni yozmasa ham bo'laveradi
-        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/learn_java_jdbc", "postgres", "1"); // 2 - step (Get connection -> db blan connection o'rnatish)
+        return DriverManager.getConnection(dbUrl, dbUser, dbPassword); // 2 - step (Get connection -> db blan connection o'rnatish)
     }
 }
